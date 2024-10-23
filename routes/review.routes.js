538ca62733fae037b2069be1c14a2f18378df421
Anik3146 <anikweb153@gmail.com>
2024-10-23 21:52:49 +0600

    token requirement reduced for review api

--- routes/review.routes.js
+++ routes/review.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { addReview, deleteReviews } = require("../controller/review.controller");
 const verify = require("../middleware/verifyToken"); // Make sure to import your verify middleware
 
-router.use(verify); // Apply the verify middleware to all routes below
+//router.use(verify); // Apply the verify middleware to all routes below
 
 // add a review
 router.post("/add", addReview);
