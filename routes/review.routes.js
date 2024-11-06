62ba9f430f7c545fb630e683abc8666335fae394
Anik3146 <anikweb153@gmail.com>
2024-11-06 23:22:31 +0600

    improved review api

--- routes/review.routes.js
+++ routes/review.routes.js
@@ -7,10 +7,11 @@ const {
 } = require("../controller/review.controller");
 const verify = require("../middleware/verifyToken"); // Make sure to import your verify middleware
 
-//router.use(verify); // Apply the verify middleware to all routes below
-router.use(verify);
 // add a review
 router.post("/add", addReview);
+
+//router.use(verify); // Apply the verify middleware to all routes below
+router.use(verify);
 // delete reviews
 router.delete("/delete/:id", deleteReviews);
 
