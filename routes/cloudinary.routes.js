bc5f60112389385ded2f7e9944d50e58c443a08a
DESKTOP-O2KROQH\User <anikweb153@gmail.com>
2024-11-13 23:13:40 +0600

    vercel cors

--- routes/cloudinary.routes.js
+++ routes/cloudinary.routes.js
@@ -44,7 +44,15 @@ const upload = multer({
 
 // Upload image to Cloudinary endpoint
 router.post("/add", upload.single("image"), async (req, res) => {
-  
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
   if (!req.file) {
     return res.status(400).json({
       success: false,
